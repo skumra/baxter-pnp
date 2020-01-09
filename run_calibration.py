f05ab182b9b2796dee337fc8e14c55952e0198c6
Sulabh Kumra <sulabh@osaro.com>
2020-01-09 10:42:29 -0800

    Fixed tool_orientation

--- run_calibration.py
+++ run_calibration.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-import numpy as np
 
 from tasks.calibrate import Calibration
 
 if __name__ == '__main__':
-    calibration = Calibration(tool_orientation=[-np.pi / 2, 0, 0])
+    calibration = Calibration(tool_orientation=[1, 0, 0, 0])
     calibration.run()
