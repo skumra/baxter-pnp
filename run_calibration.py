94b7c50a052c81077c5a6b5ea7cfd5a08d3fd2b6
ShirinJ <saj8732@rit.edu>
2020-02-21 09:15:09 -0800

    calibration fixes

--- run_calibration.py
+++ run_calibration.py
@@ -3,5 +3,5 @@
 from tasks.calibrate import Calibration
 
 if __name__ == '__main__':
-    calibration = Calibration(tool_orientation=[1, 0, 0, 0])
+    calibration = Calibration()
     calibration.run()
